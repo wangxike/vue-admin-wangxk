805855555df2ef664e4a4323b2f031286da93d7d
wangxike <sxwangxike@163.com>
2020-01-04 15:54:34 +0800

    写法

--- src/pages/order/List.vue
+++ src/pages/order/List.vue
@@ -11,8 +11,8 @@
             <el-tab-pane label="待确认" name="seventh"></el-tab-pane>
             <el-tab-pane label="已完成" name="eightth"></el-tab-pane>
         </el-tabs>
-         <!--标签页-->
-          <!--表格-->
+        <!--标签页-->
+        <!--表格-->
         <el-table :data="orders.list">
             <el-table-column prop="id" label="编号"></el-table-column>
             <el-table-column width="150" prop="orderTime" label="下单时间"></el-table-column>
@@ -25,40 +25,32 @@
                 <template v-slot="slot">
                     <a href="" @click.prevent="toDeleteHandler(slot.row.id)">删除</a>
                     <a href="" @click.prevent="toUpdateHandler(slot.row)">修改</a>
-
                 </template>
-
             </el-table-column>
         </el-table>
-         <!--表格结束-->
-         <!--分页开始-->
-         <el-pagination layout="prev, pager, next" :total="orders.total" @current-change="pageChangeHandler">
-         </el-pagination>
-         <!--分页结束-->
-         <!--模态框-->
-         <el-dialog
-        title="录入订单信息"
-        :visible.sync="visible"
-        width="60%">
+        <!--表格结束-->
+        <!--分页开始-->
+        <el-pagination layout="prev, pager, next" :total="orders.total" @current-change="pageChangeHandler"/>
+        <!--分页结束-->
+        <!--模态框-->
+        <el-dialog title="录入订单信息" :visible.sync="visible" width="60%">
         ---{{form}}
             <el-form :model="form" label-width="80px">
                 <el-form-item label="下单时间">
-                <el-input v-model="form.orderTime"></el-input> 
+                    <el-input v-model="form.orderTime"></el-input> 
                 </el-form-item> 
             </el-form>
-
             <el-form :model="form" label-width="80px">
-                 <el-form-item label="总价">
-                <el-input v-model="form.total"></el-input>
+                <el-form-item label="总价">
+                    <el-input v-model="form.total"></el-input>
                 </el-form-item>
             </el-form>  
 
             <el-form :model="form" label-width="80px">
-                 <el-form-item label="状态">
-                <el-input v-model="form.status"></el-input>
+                <el-form-item label="状态">
+                    <el-input v-model="form.status"></el-input>
                 </el-form-item>
             </el-form>     
-
         <span slot="footer" class="dialog-footer">
             <el-button size="small" @click="closeMedalHandler">取 消</el-button>
             <el-button size="small" type="primary" @click="submitHander">确 定</el-button>
@@ -74,40 +66,41 @@ export default {
     //用于存放网页中需要存放的方法
     methods:{
         loadData(){
-             let url="http://localhost:6677/order/queryPage"
-             request({
+            let url="http://localhost:6677/order/queryPage"
+            request({
                 url,
                 method:"post",
                 headers:{
                    "Content-Type":"application/x-www-form-urlencoded"
                 },
                 data:querystring.stringify(this.params)
-             }).then((response)=>{//指向外部this
+            }).then((response)=>{//指向外部this
                 //将查询结果设置到order，orders为一个对象
                 this.orders=response.data;
-             })
-        },
-        submitHander(){
-            let url="http://localhost:6677/order/save"
-                request({
-                  url,
-                  method:"POST",
-                  headers:{
-                   "Content-Type":"application/x-www-form-urlencoded"
-                    },
-                data:querystring.stringify(this.form)
-                }).then((response)=>{
-                 this.closeMedalHandler();
-                 this.loadData();
-                 this.$message({
-                      type:"success",
-                    message:response.message
-                })
             })
         },
-    
+
+        submitHander(){
+            let url="http://localhost:6677/order/save"
+            request({
+                url,
+                method:"POST",
+                headers:{
+                    "Content-Type":"application/x-www-form-urlencoded"
+                },
+                data:querystring.stringify(this.form)
+            }).then((response)=>{
+                this.closeMedalHandler();
+                this.loadData();
+                this.$message({
+                    type:"success",
+                    message:response.message
+                });
+            });
+        },
+
         toDeleteHandler(id){
-                //确认
+            //确认
             this.$confirm('此操作将永久删除该条信息, 是否继续?', '提示', {
                 confirmButtonText: '确定',
                 cancelButtonText: '取消',
@@ -122,26 +115,26 @@ export default {
                         type: 'success',
                         message: response.message
                     });
-                })
-            
+                });
             })
         },
 
         toUpdateHandler(row){
             this.form=row;
             this.visible=true;
-
         },
+
         closeMedalHandler(){
             this.visible=false;
-
         },
+
         toAddHandler(){
             this.form={
                 type:"order"
             }
             this.visible=true;
         },
+
         pageChangeHandler(page){
             this.params.page=page-1;
             this.loadData();
@@ -162,12 +155,12 @@ export default {
             }
         }
     },
-   
+
     created(){
         // this为当前vue实例对象
         // vue实例创建完毕 
         this.loadData();
- 
+        
     }
 }
 </script>
